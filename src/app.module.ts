b457bd538638f6600b4c79c36c65ff7ca536800f
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-10-21 09:56:56 -0300

    feat: Apollo Server added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+    }),
+    ItemsModule,
+  ],
   controllers: [],
   providers: [],
 })
