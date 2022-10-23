454e64f2d04a400c156ceb0259ea9cdb61256cf0
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-10-23 00:08:27 -0300

    feat: Logs errors in users service added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -7,6 +12,8 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private logger = new Logger('UsersService');
+
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
@@ -17,8 +24,7 @@ export class UsersService {
       const newUser = await this.usersRepository.create(signupInput);
       return await this.usersRepository.save(newUser);
     } catch (error) {
-      console.error(error);
-      throw new BadRequestException(`Error...`);
+      this.handleDBErrors(error);
     }
   }
 
@@ -34,5 +40,13 @@ export class UsersService {
   async block(id: string): Promise<User> {
     const newUser = new User();
     return newUser;
+  }
+
+  private handleDBErrors(error: any): never {
+    this.logger.error(error);
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail.replace('Key ', ''));
+    }
+    throw new InternalServerErrorException('Please check server logs');
   }
 }
