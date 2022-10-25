539bf47fe6dc0d6057463ad1871b4614cb8c4ed3
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-10-25 00:47:18 -0300

    feat: JWT Strategy added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { UsersModule } from 'src/users/users.module';
+import { JwtStrategy } from './strategies';
 
 @Module({
-  providers: [AuthResolver, AuthService],
-  imports: [UsersModule],
+  providers: [AuthResolver, AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule, JwtModule],
+  imports: [
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '4h',
+        },
+      }),
+    }),
+    UsersModule,
+  ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import * as bcrypt from 'bcrypt';
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 import { UsersService } from './../users/users.service';
 import { SigninInput, SignupInput } from './dto';
@@ -7,12 +8,18 @@ import { AuthResponse } from './types';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  private getJwtToken(userId: string) {
+    return this.jwtService.sign({ id: userId });
+  }
 
   async signup(signupInput: SignupInput): Promise<AuthResponse> {
     const user = await this.usersService.create(signupInput);
-    // TODO: token
-    const token = '123';
+    const token = this.getJwtToken(user.id);
     return { user, token };
   }
 
@@ -21,8 +28,7 @@ export class AuthService {
     if (!bcrypt.compareSync(password, user.password)) {
       throw new BadRequestException('Email / Password do not match');
     }
-    // TODO: token
-    const token = '123';
+    const token = this.getJwtToken(user.id);
     return { user, token };
   }
 }
