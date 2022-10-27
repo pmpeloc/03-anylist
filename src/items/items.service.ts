5ce0cc7db586df34fa56d725118213fd03b61a70
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-10-26 23:49:19 -0300

    feat: Update Item for user added

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -34,7 +34,12 @@ export class ItemsService {
     return item;
   }
 
-  async update(id: string, updateItemInput: UpdateItemInput): Promise<Item> {
+  async update(
+    id: string,
+    updateItemInput: UpdateItemInput,
+    user: User,
+  ): Promise<Item> {
+    await this.findOne(id, user);
     const item = await this.itemsRepository.preload(updateItemInput);
     if (!item) throw new NotFoundException(`Item whith id: ${id} not found`);
     return this.itemsRepository.save(item);
