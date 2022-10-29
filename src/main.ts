e3eca815031a158cf87866fc29804f84659b8554
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-10-29 00:23:11 -0300

    feat: Postgres Digital Ocean added

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
       // forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT || 3000);
+  console.log(`API AnyList running on port ${PORT}`);
 }
 bootstrap();
